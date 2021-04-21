8ad9d43c65130f915f6c0f9a89ce0d4cdb0be708
Evgeny D <evgeniy_98-08@live.ru>
2021-04-21 13:30:19 +0300

    fix - add ReportCode enum

---
+++ ReportEntities/Enums/ReportCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportEntities.Enums
+{
+    public enum ReportCode
+    {
+        MoveAndStop = 1,
+        MessagesFromObject = 2
+    }
+}
---
+++ ReportEntities/IReportHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ReportEntities.Reports;
+
+namespace ReportEntities
+{
+    public interface IReportHandler
+    {
+        public void Execute(Report report);
+    }
+}
